f6febd40cb6936ea19f3d6ce67867083adbb76ff
sayya <djdj9876@naver.com>
2023-08-09 19:07:20 +0900

    Project - Problem5 기능 구현 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,22 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static List<Integer> coins = new ArrayList<>();
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        List<Integer> coins = List.of(50_000,10_000,5_000,1_000,500,100,50,10,1);
+
+        List<Integer> ans = new ArrayList<>();
+        for (Integer coin : coins) {
+            ans.add(Math.floorDiv(money, coin));
+            money -= coin * (Math.floorDiv(money, coin));
+        }
+        return ans;
     }
-}
+
+
+} // class problem5
