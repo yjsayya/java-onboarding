cd928f8dce7d935a188180916c94fda4c6256756
sayya <djdj9876@naver.com>
2023-08-09 18:59:45 +0900

    Project - Problem3 기능 구현 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int cnt = 0;
+        for (int i = 1; i <= number; i++) {
+            cnt += count(Integer.toString(i));
+        }
+        return cnt;
     }
+
+    public static int count(String num) {
+        int cnt = 0;
+        for (int i = 0; i < num.length(); i++) {
+            if (num.charAt(i) == '3' || num.charAt(i) == '6' || num.charAt(i) == '9')
+                cnt++;
+        }
+        return cnt;
+    }
+
+
 }
